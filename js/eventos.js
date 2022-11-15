8a35cca350b08f6ec84c3ec909c534fcb9b2a863
Silvio Arem <silvio.arem@gmail.com>
2022-11-14 22:41:22 -0300

    consumindo a API e listando na página eventos

---
+++ js/eventos.js
@@ -0,0 +1,24 @@
+const section = document.querySelector("#containerEventos");
+
+async function getEventos() {
+    const response = await fetch(`https://xp41-soundgarden-api.herokuapp.com/events
+    `);
+
+    const eventos = await response.json();
+
+    eventos.forEach(evento => {
+        const cardEvento = `
+        <article class="evento card p-5 m-2 ">
+            <h2>${evento.name}</h2>
+            <h4>${evento.attractions}</h4>
+            <p >${evento.description}</p>
+            <a  href="#" class="btn btn-primary">reservar ingresso</a>
+        </article>`;
+
+        section.innerHTML += cardEvento;
+    })
+
+    console.log(eventos);
+}
+
+getEventos();
