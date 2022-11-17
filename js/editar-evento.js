b5dcd897527403f52b280f64e934d95fbe9a4297
Silvio Arem <silvio.arem@gmail.com>
2022-11-17 13:03:07 -0300

    Alimentado o formulário de edição de evento com os dados da API

--- js/editar-evento.js
+++ js/editar-evento.js
@@ -1,1 +1,35 @@
-console.log(`Oi, Lindo`)
+const URL_EVENT_ID = "https://xp41-soundgarden-api.herokuapp.com/events/";
+const URL_ATUAL = window.location.search;
+let parametros = new URLSearchParams(URL_ATUAL);
+const ID = parametros.get("id");
+let inputNome = document.querySelector("#nome");
+let inputBanner = document.querySelector("#banner");
+let inputAtracoes = document.querySelector("#atracoes");
+let inputDescricao = document.querySelector("#descricao");
+let inputData = document.querySelector("#data");
+let inputLotacao = document.querySelector("#lotacao");
+let btnExcluir = document.querySelector("#btnExcluir");
+
+
+
+async function getEvento(ID) {
+
+    try {
+        const resposta = await fetch(URL_EVENT_ID + ID);
+
+        const dadosEvento = await resposta.json();
+
+        inputNome.value = dadosEvento.name;
+        inputBanner.value = dadosEvento.poster;
+        inputAtracoes.value = dadosEvento.attractions;
+        inputDescricao.value = dadosEvento.description;
+        inputData.value = new Date(dadosEvento.scheduled).toLocaleString('pt-BR', { timeZone: "America/Sao_Paulo", dateStyle: "short", timeStyle: "short" });
+        inputLotacao.value = dadosEvento.number_tickets;
+    } catch(error) {
+        console.log("Erro ao encontrar link");
+    }
+
+}
+
+getEvento(ID);
+
